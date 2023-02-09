a4321f6a5e612196a2fa0e4bfe6a74e9d4a79fc0
marvynmesquita <31486769+marvynmesquita@users.noreply.github.com>
2023-02-09 10:38:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ app.use(express.json())
 app.options('*', cors())
 //cors
 app.use((req, res, next) => {
-	res.header("access-control-allow-origin", "*",
-        'Access-Control-Allow-Headers', 'Content-Type, Authorization',
-        'Access-Control-Allow-Methods', '*',
-      )
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", '*');
+    res.header("Access-Control-Allow-Headers", 'Content-Type, Authorization');
     app.use(cors());
     next();
 });
