0075b4dd2af61746683f42bf952f0d12ab795815
marvynmesquita <31486769+marvynmesquita@users.noreply.github.com>
2023-02-09 10:15:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,17 +14,11 @@ app.use(
 )
 
 app.use(express.json())
-app.options('*', cors())
 //cors
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'POST, GET, PUT, DELETE, OPTIONS, HEAD, authorization');
-    res.header("Access-Control-Allow-Headers", "content-type");
-    app.use(cors());
-    next();
-});
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 //rotas da api
 
