fbdbc72e0fdae59141c89fdcd47844050077b45f
marvynmesquita <31486769+marvynmesquita@users.noreply.github.com>
2023-02-09 10:23:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(
 
 app.use(express.json())
 //cors
+app.options('/product/:id', cors()) // enable pre-flight request for DELETE request
+app.del('/product/:id', cors(), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 app.use(cors({
     origin: '*',
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
